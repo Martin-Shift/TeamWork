838a7c426aa72cdcb34261cadbbd014f48be1d87
Martin Saprykin <sapr_dp83@student.itstep.net>
2022-06-07 19:21:39 +0200

    Slowed Hello print

--- TeamWork.cpp
+++ TeamWork.cpp
@@ -1,10 +1,15 @@
-
-
 #include <iostream>
-
+#include <thread>
+#include <string>
+#include <chrono>
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string Hello = "Hello World!\n";
+    for (int i{}; i < Hello.size(); i++)
+    {
+        std::cout << Hello[i];
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
 }
 
 
